5932a87cebb086726d1e98b9f19a51bfa35bacef
Viktoriia <vika.vinnikova@gmail.com>
2025-06-13 14:19:30 +0200

    problems added

---
+++ Pages/TempConverter.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+import Cocoa
+import Foundation
+
+// Fahrenheit to Celsius Converter
+
+// Write a Swift function that:
+// - takes a single Double parameter representing a temperature in Fahrenheit.
+// - calculates the equivalent temperature in Celsius.
+// - returns the Celsius temperature as a Double.
+// hint: the formula for converting Fahrenheit (F) to Celsius (C) is
+// C=(F−32)×5/9
+
+func tempConverter(tempFahrenheit: Double) -> Double {
+    var tempCelsius: Double = (tempFahrenheit - 32) * 5 / 9
+    return tempCelsius
+}
+
+// testing
+
+let fahrenheitTest: Double = 68.0
+let convertedCelsius = tempConverter(tempFahrenheit: fahrenheitTest)
+
+print("Temperature in Fahrenheit: \(fahrenheitTest)°F, Temperature in Celsius: \(convertedCelsius)°C")
